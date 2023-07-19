6d179116be50a9b89b241bf99e8a6498b8703549
Balram Gupta <109209618+balramg7@users.noreply.github.com>
2023-07-19 16:29:37 +0530

    changed according to Question

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,16 @@ const App = () => {
 
   const handleClick = (event) =>{
     // use console.log
+    const buttonId = event.target.id;
+    console.log(`Button id is: ${buttonId}`);
 
   }
 
   // do not remove the two buttons or change their id
   return (
     <div id="main">
-      <button id="button-a">Button A</button>
-      <button id="button-b">Button B</button>
+      <button id="button-a" onClick={this.handleClick}>Button A</button>
+      <button id="button-b" onClick={this.handleClick}>Button B</button>
     </div>
   )
 }
